23dad244c7a10e4b72bdeeab84d7e56f48ed0aed
yassine-ea <y.elaboubi@gmail.com>
2023-12-25 02:52:51 +0100

    WorkStation - #0001

--- js/app.js
+++ js/app.js
@@ -1,12 +1,20 @@
 const secondes = document.querySelector(".secondes");
 const minutes = document.querySelector(".minutes");
 const hours = document.querySelector(".hours");
-setInterval(timeDisplay, 1000);
+function Func() {
+	fetch("../city.json")
+		.then((res) => {
+			return res.json();
+		})
+		.then((data) => console.log(data));
+}
+Func();
+setInterval(timeDisplay(4), 1000);
 function timeDisplay(timezone) {
-	let sec = new Date().getSeconds().toString();
-  let min = new Date().getMinutes().toString();
-  let hr = new Date().getHours().toString();
+	let sec = new Date().getUTCSeconds().toString();
+	let min = new Date().getUTCMinutes().toString();
+	let hr = new Date().getUTCHours().toString();
 	secondes.style.transform = `rotateZ(${sec * 6}deg)`;
 	minutes.style.transform = `rotateZ(${min * 6}deg)`;
-  hours.style.transform = `rotateZ(${(parseInt(hr) +0)* 30}deg)`;
+	hours.style.transform = `rotateZ(${(parseInt(hr) + 1) * 30}deg)`;
 }
